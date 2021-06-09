4e0a9690cb9c59b12f4267d466d31ae2a2e6642c
Anul Agarwal <anulagarwal12@gmail.com>
2021-06-09 11:58:39 +0530

    GameManger & UIManager Updated

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance = null;
+
+    public enum State { MainMenu, InGame, Win, Lose };
+    #region Attributes
+
+    [Header("Attributes")]
+    public bool isPaused;
+
+    [Header("Component Reference")]
+    public GameObject confetti;
+
+    [Header("GameplayStates")]
+    public State gameState;
+
+    #endregion
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+
     // Start is called before the first frame update
+
+    #region Monobehaviour functions
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    #endregion
+
+
+    public void StartLevel()
+    {
+        ChangeState(State.InGame);
+    //    PlayerController.Instance.Fall();
+    }
+
+    public void WinLevel()
+    {
+
+       // PlayerController.Instance.Victory();
+        ChangeState(State.Win);
+        confetti.SetActive(true);
+    }
+
+    public void LoseLevel()
+    {
+
+    }
+
+    public void LoadLevel(string s)
+    {
+        SceneManager.LoadScene(s);
+    }
+
+    public void ChangeState(State state)
+    {
+        gameState = state;
+        UIManager.Instance.UpdateState(state);
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public static UIManager Instance = null;
+
+    [Header("Panel References")]
+    public GameObject MainMenu;
+    public GameObject InGame;
+    public GameObject WinPanel;
+    public GameObject LosePanel;
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void UpdateLevelText()
+    {
+
+    }
+
+    public void UpdateState(GameManager.State state)
+    {
+        switch (state)
+        {
+            case GameManager.State.MainMenu:
+                MainMenu.SetActive(true);
+                InGame.SetActive(false);
+                WinPanel.SetActive(false);
+                LosePanel.SetActive(false);
+                break;
+
+            case GameManager.State.InGame:
+                MainMenu.SetActive(false);
+                InGame.SetActive(true);
+                WinPanel.SetActive(false);
+                LosePanel.SetActive(false);
+                break;
+
+            case GameManager.State.Win:
+                MainMenu.SetActive(false);
+                InGame.SetActive(false);
+                WinPanel.SetActive(true);
+                LosePanel.SetActive(false);
+                break;
+
+            case GameManager.State.Lose:
+                MainMenu.SetActive(false);
+                InGame.SetActive(false);
+                WinPanel.SetActive(false);
+                LosePanel.SetActive(true);
+                break;
+
+        }
     }
 }
